56f19edc2dcc13eebb45ccb83a1fdb751c76243d
JavaHunk2020 <javahunk2020@gmail.com>
2023-06-02 05:05:27 +0530

    login page design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'login-page';
+    title = 'login-page';
+    message:string="";  
+    public validate(username:any,password:any) : void {
+          if(username.value==='jack' && password.value==='jill'){
+             this.message="Ahaha usernane and password are correct!";
+          }else{
+            this.message="Sorry usernane and password are not correct!";
+          }
+         console.log("Hey you clicked me!");   
+    }
+
 }
