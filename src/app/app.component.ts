d03ecd7d7041fa5f65a2e6208f21695ab29578b4
JavaHunk2020 <javahunk2020@gmail.com>
2023-06-03 04:47:53 +0530

    Rest API Integration with angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { AppResponse } from './app.response.model';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -8,13 +11,18 @@ import { Component } from '@angular/core';
 export class AppComponent {
     title = 'login-page';
     message:string="";  
-    public validate(username:any,password:any) : void {
-          if(username.value==='jack' && password.value==='jill'){
-             this.message="Ahaha usernane and password are correct!";
-          }else{
-            this.message="Sorry usernane and password are not correct!";
-          }
-         console.log("Hey you clicked me!");   
+
+    public constructor(private httpClient : HttpClient){
     }
 
+    public validate(pusername:any,ppassword:any) : void {
+        const reqeuestBody={username:pusername.value,password:ppassword.value};
+        //HERE WE ARE MAKING REST CALL
+        let response:Observable<AppResponse> = 
+        this.httpClient.post<AppResponse>("http://localhost:9090/v4/auth",reqeuestBody);
+        response.subscribe((data:AppResponse)=>{
+            console.log(data);
+            this.message= data.message;
+        });
+    }
 }
---
+++ src/app/app.response.model.ts
@@ -0,0 +1,6 @@
+
+export interface AppResponse{
+    code:string;
+    message:string;
+    created:Date;
+}
